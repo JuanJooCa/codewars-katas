97e48b48f7b3f9e785959acef503a2497fbe3d6e
alumno <oscarmaller43@gmail.com>
2026-09-23 14:03:47 +0200

    feat(multi_table-for_number): mostrar tabla de un numero

---
+++ multi_table_for_numer.scala
@@ -0,0 +1,9 @@
+def multiTable(n: Int): String =  {
+  val n = 5
+  var multiplo = 1
+  var resultado = multiplo * n
+  {if resultado >= 5 then 
+  println(s"Final: $multiplo * $n = + $resultado")
+  }
+  
+}
